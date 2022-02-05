cb007d556297da7313f2e69c3e2cd73075a15b5b
Prathmesh <joship@gmail.com>
2022-02-05 16:46:45 +0530

    Init commmit

---
+++ src/app.js
@@ -0,0 +1,133 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+// console.log(__dirname);
+// console.log(path.join(__dirname, "../public"));
+
+const app = express();
+
+// Define Paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlers engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup Static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Prathmesh Joshi",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Prathmesh joshi",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Your Helping Hands are Here...",
+    name: "Prathmesh joshi",
+    tollfreenumber: "8800089",
+  });
+});
+// app.get("", (req, res) => {
+//   res.send("<h1>Weather</h1>");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.send([
+//     {
+//       name: "Prathmesh",
+//       age: 22,
+//     },
+//     {
+//       name: "Joshi",
+//       age: 25,
+//     },
+//   ]);
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("<h2><u>About Page</u></h2>");
+// });
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address !",
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+
+  // res.send({
+  //   forecast: "It is cold",
+  //   location: "Nikora",
+  //   address: req.query.address,
+  // });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Prathmesh joshi",
+    errorMessage: "Help Article not Found !",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Prathmesh joshi",
+    errorMessage: "Page not found",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
+});
+4;
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&units=metric&appid=c5060ebccc4d3361e2f24328c6851710";
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service !", undefined);
+    } else if (body.message) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(undefined, "Currently " + body.main.temp + " Celcius Now !");
+    }
+  });
+};
+
+module.exports = forecast;
